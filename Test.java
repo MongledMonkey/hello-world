9ab4c495d1f8edee8171734427f593893c89410a
monkey <monkey@Monkeybox.local>
2015-09-07 22:38:18 -0700

    Initial import

---
+++ Test.java
@@ -0,0 +1,13 @@
+// Test comment
+/* other comment */
+
+import java.io.*;
+
+class Test
+{
+  public static void main( String[] args )
+  {
+		String a = new String( "test" );
+		System.out.println( a );
+  }
+}
